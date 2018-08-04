e140f17fe9631ab2169abc17900de266ae9f9fcf
JohariGourav <41551799+JohariGourav@users.noreply.github.com>
2018-08-04 14:18:13 +0530

    added jar testing files

---
+++ src/TestEmployee.java
@@ -0,0 +1,12 @@
+//Class TestEmployee to import Emp using Jar file in default package
+public class TestEmployee {
+
+	// main method
+	public static void main(String[] args) {
+
+		Emp emp = new Emp(1, "Nitin", 15000);
+		emp.disp();
+
+	}
+
+}
---
+++ src/TestOrg.java
@@ -0,0 +1,15 @@
+
+//Class TestOrg to import Org using Jar file in pack2
+import pack2.Org;
+
+public class TestOrg {
+
+	// main method
+	public static void main(String[] args) {
+
+		Org org = new Org(1052, "Dell", 10000000);
+		org.disp();
+
+	}
+
+}
